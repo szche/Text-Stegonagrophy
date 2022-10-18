6d32f0e270359234fc0406a0382289a47d3dec3c
szche <szymon@chadam.pl>
2022-10-18 15:16:01 +0200

    Change window size and small css fix

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const path = require('path')
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 1000,
+    center: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
